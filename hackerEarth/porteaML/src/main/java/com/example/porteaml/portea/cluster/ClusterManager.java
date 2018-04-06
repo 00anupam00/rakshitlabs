fec194852a6549fd365cb67c882c4c0f482a6fd5
Anupam Rakshit <arakshit@zaloni.com>
2018-04-06 19:20:14 +0530

    ML structure project

---
+++ hackerEarth/porteaML/src/main/java/com/example/porteaml/portea/cluster/ClusterManager.java
@@ -0,0 +1,28 @@
+package com.example.porteaml.portea.cluster;
+
+import org.apache.spark.SparkConf;
+import org.apache.spark.sql.SparkSession;
+
+public class ClusterManager {
+
+    private static ClusterManager ourInstance = new ClusterManager();
+    private static SparkSession spark;
+
+    public static ClusterManager getInstance() {
+        return ourInstance;
+    }
+
+    private ClusterManager() {
+
+        spark= SparkSession.builder()
+                                        .master("local[*]")
+                                        .appName("Predictive Modelling")
+                                        .config(new SparkConf())
+                                        .getOrCreate();
+    }
+
+    public static SparkSession getSparkSession(){
+        getInstance();
+        return spark;
+    }
+}
---
+++ hackerEarth/porteaML/src/main/java/com/example/porteaml/portea/regression/MultinomialLogisticRegr.java
@@ -0,0 +1,13 @@
+package com.example.porteaml.portea.regression;
+
+import org.apache.spark.ml.PipelineModel;
+import org.apache.spark.sql.Dataset;
+import org.apache.spark.sql.Row;
+
+public class MultinomialLogisticRegr {
+
+    public PipelineModel prepareModel(Dataset<Row> dataset){
+        //Dataset<Row> trainData= dataset.sele
+        return null;
+    }
+}
